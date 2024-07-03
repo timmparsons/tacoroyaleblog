257b44681464909da146384c649603430121e706
Tim Parsons <timmparsons85@gmail.com>
2024-07-03 07:45:37 -0700

    host on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
-import netlify from "@astrojs/netlify";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: 'server',
-  adapter: netlify()
+  adapter: vercel(),
 });
